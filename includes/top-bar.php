0dc60e4b194a1ee0e6ef004b827702e4125969b5
vinod10 <vinodkumar@kwiqpick.com>
2017-07-23 19:00:31 +0530

    separated index files into bits

---
+++ includes/top-bar.php
@@ -0,0 +1,22 @@
+<main class="content-wrapper oh">
+
+    <!-- Navigation -->
+    <header class="nav-type-1">
+
+        <div class="top-bar hidden-sm hidden-xs">
+            <div class="container">
+                <div class="top-bar-line">
+                    <div class="row">
+                        <div class="top-bar-links">
+                            <ul class="col-sm-6 top-bar-acc">
+                                <li class="top-bar-link"><a href="#">My Account</a></li>
+                                <li class="top-bar-link"><a href="#">My Wishlist</a></li>
+                                <li class="top-bar-link"><a href="#">Login</a></li>
+                                <li class="top-bar-link"><a href="">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div> <!-- end top bar -->
