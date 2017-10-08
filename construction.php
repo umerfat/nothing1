0619afe45317431b9de4e87b0acb1faab9fa6015
vinod10 <vinodkumar@kwiqpick.com>
2017-10-08 17:48:37 +0530

    added simple construction page

---
+++ construction.php
@@ -0,0 +1,21 @@
+<?php include "includes/header.php"; ?>
+
+<?php include "includes/top-bar.php"; ?>
+
+<?php include "includes/nav.php"; ?>
+
+<!-- 404 -->
+<section class="section-wrap page-404">
+    <div class="container">
+
+        <div class="row text-center">
+            <div class="col-md-6 col-md-offset-3">
+                <h2 class="mb-50">This page is under construction</h2>
+            </div>
+        </div>
+
+    </div>
+</section> <!-- end 404 -->
+
+<?php include "includes/footer.php"; ?>
+</html>
