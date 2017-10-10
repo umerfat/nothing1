7d18729ad87a2ee826f39e22693179953fe8acb0
umerhurrah <umerhurrah@kwiqpick.com>
2017-10-10 09:41:48 +0530

    pushing changes from UI branch

--- admin/categories.php
+++ admin/categories.php
@@ -23,21 +23,33 @@
                         <!--Calling php function for inserting categories-->
                         <?php insert_category(); ?>
 
-                        <form class="form-horizontal form-label-left" method="post"
+                        <form class="form-label-left" method="post"
                               id="add-category-form">
                             <div class="item form-group">
-                                <label class="control-label col-md-3 col-sm-3 col-xs-12"
-                                       for="category">Add Category <span
-                                            class="required">*</span>
-                                </label>
-                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                <div class="col-md-6 col-sm-12 col-xs-12">
                                     <input id="name" class="form-control col-md-7
                                     col-xs-12" name="cat_name" placeholder="e.g Mason"
                                            type="text">
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-3">
+                            <div class="item form-group">
+                                <div class="col-md-3 col-sm-12 col-xs-12">
+                                    <input id="name" class="form-control col-md-7
+                                    col-xs-12" name="cat_name" placeholder="e.g Mason"
+                                           type="text">
+                                </div>
+                                <div class="col-md-3 col-sm-12 col-xs-12">
+                                    <select class="form-control" name="category" required>
+                                        <option value="">Choose Sub Category</option>
+                                        <option value="1">Mason</option>
+                                        <option value="2">Carpenter</option>
+                                        <option value="3">Farmer</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="item form-group">
+                                <div class="col-md-6">
                                     <button class="btn btn-primary"
                                             type="reset">Reset</button>
                                     <button id="submit" name="submit_category"
@@ -46,8 +58,8 @@
                                     </button>
                                 </div>
                             </div>
-                            <div class="ln_solid"></div>
                         </form>
+                        <div class="ln_solid"></div>
                         <?php
                         //update and include on clicking edit link
                         if (isset($_GET['edit'])){
